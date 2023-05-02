a71a483993e491e7cef805e7cc1670ec94413e08
nancy1827 <np182710@gmail.com>
2023-05-02 16:09:23 +0530

    first commit

---
+++ userdefine.py
@@ -0,0 +1,65 @@
+##n=int(input("Enter the number:"))
+##if n<100:
+##    print(n,"is less than 100")
+##else:
+##    print(n,"is greater than 100")
+##    print("thanks")
+
+
+##food = "chaumin"
+##
+##if food == "kabab":
+##    print("ummm, my favorite!")
+##    print("i feel like saying it 100 times..")
+##    print(100 * (food + " ! "))
+##else:
+##    print("im hungry")
+
+##num = int(input("enter any number:"))
+##flag=num%2
+##if flag ==0:
+##    print(num,"is an even number")
+##
+##elif flag ==1:
+##    print(num,"is an odd number")
+##else:
+##    print("error ,invalid input")
+##a=(input("enter your name:"))
+##print("my name is",a)
+
+##x =input("enter first number")
+##y= input("enter second number")
+##print(x+y)
+##
+##print(int(x)+int(y))
+##
+
+##name = "harry"
+##friend = "rohan"
+##anotherfriend ="nancy"
+##apple = ''' he said,
+##ghfryyewedf
+##ds
+##"hyhrtyhsertg'''
+##print( name)
+###print(apple)
+##print(name[0])
+##print("lets use a for loop\n")
+##for character in apple:
+##    print(character)
+
+   # doneeeeee
+
+##names = "harry,shubham"
+##print(names[0:6])
+##
+##names = "harry,shubham"
+##print(len(names))
+
+##fruit ="Mango"
+##mangoLen = len(fruit)
+##print(mangoLen)
+##print(fruit[0:4])
+##
+##
+##
